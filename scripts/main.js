03fd60d146d5bf14e8bbbb495f1be21bcefb4813
ArchivexBlasich <fblasich0@gmail.com>
2025-08-07 09:47:57 -0300

    Add basic dialog and form basic work,  let save a book in the library from the dialog

--- scripts/main.js
+++ scripts/main.js
@@ -15,13 +15,95 @@ function Book(title, author, pages, read) {
     };
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
+function addBookToLibrary(book) {
+    myLibrary.push(new Book(book.title, book.author, book.pages, book.read));
+}
+
+function showBooksInLibrary(library) {
+    console.table(library);
+}
+
+/*-------- Auxiliary Functions ----------*/
+function getBookFromInputs(inputs) {
+    let bookTitle, bookAuthor, bookPages, bookRead;
+    let book = [];
+    for (const input of inputs) {
+        if (input.name === "bookRead") {
+            book.push(input.checked);
+            continue;
+        }
+        book.push(input.value);
+    }
+    
+    [bookTitle, bookAuthor, bookPages, bookRead] = book;
+
+    return {
+        title: bookTitle,
+        author: bookAuthor,
+        pages: bookPages,
+        read: bookRead,
+    };
 }
 
 
-addBookToLibrary("Don Quijote", "Milei", "267", false);
-addBookToLibrary("Fundamentos de Redes", "Saade", "350", true);
-addBookToLibrary("Sistemas operativos", "Dinosaurio", "1000", false);
+/*------------- Page general functionality and DOM manipulation ----------------- */
+const newBookBtn = document.querySelector(".new-book-btn");
 
-console.log(myLibrary);
+const modal = document.querySelector("#new-book-modal");
+
+const bookTitleInput = document.querySelector("dialog input[name='bookTitle']");
+const bookAuthorInput = document.querySelector("dialog input[name='bookAuthor']");
+const bookPagesInput = document.querySelector("dialog input[name='book-pages']");
+const bookReadInput = document.querySelector("dialog input[name='bookRead']");
+
+const addBookModalBtn = document.querySelector(".add-book-modal-btn");
+const cancelModalBtn = document.querySelector(".cancel-modal-btn");
+
+newBookBtn.addEventListener("click", () => modal.showModal());
+cancelModalBtn.addEventListener("click", (e) => {
+    /* Without an action, submitting the form via the default GET method causes a page to reload.
+     We use JavaScript to prevent the submission and close the dialog with the 
+     event.preventDefault() and HTMLDialogElement.close() methods, respectively.*/
+    e.preventDefault()
+    modal.close()
+});
+
+addBookModalBtn.addEventListener("click", (event) => {
+    let isSomeInputEmpty = false;
+    let formInputs = document.querySelectorAll("dialog form > div input");
+    formInputs = Array.from(formInputs);
+    formInputs.forEach((input) => {
+        if (!input.value) {
+            isSomeInputEmpty = true;
+        }
+    });
+    if (isSomeInputEmpty === true) {
+        return;
+    } else {
+        event.preventDefault(); // We don't want to submit this fake form
+        let book = getBookFromInputs(formInputs);
+        addBookToLibrary(book);
+        modal.close("Book was added"); // Have to send the select box value here.
+    }
+});
+
+modal.addEventListener("close", (e) => {
+    if (!modal.returnValue) {
+        return;
+    }
+    console.log(modal.returnValue)
+    console.table(myLibrary);
+});
+
+// This Event Listener close the modal when this is press out of its area
+modal.addEventListener("click", (e) => {
+    const dialogDimensions = modal.getBoundingClientRect();
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        modal.close();
+    }
+});
